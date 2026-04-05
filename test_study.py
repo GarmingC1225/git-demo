c865bfd560dcc6a58a680be4c4f46294e8223948
Garming <Garming@example.com>
2026-04-05 23:40:32 +0800

    requests库学习-post（理解data和json两种数据区别）

--- test_study.py
+++ test_study.py
@@ -1,15 +1,30 @@
 import requests
 
-headers = {"User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit"}
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
 
-url = "https://www.baidu.com"
+}
 
-r = requests.get(url, headers = headers)
+url = "https://passport2.chaoxing.com/fanyalogin"
+
+data = {
+    "uname": "13425021631",
+    "password": "thh79pjVrDfxvqtiWSeOSg==",
+    "fid": -1,
+    "refer": "http%3A%2F%2Fi.chaoxing.com%2Fbase%3Ft%3D1696689205030",
+    "t": "true",
+    "forbidotherlogin" : 0,
+    "validate": "",
+    "doubleFactorLogin": 0,
+    "independentId": 0,
+    "independentNameId": 0
+}
+
+session = requests.Session()
+r = session.post(url, data = data, headers = headers, timeout = 10)
 
 print(r.status_code)
-print(r.text[:100])
-
-
+print(r.text[:500])
 
 
 
