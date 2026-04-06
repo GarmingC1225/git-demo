7f16531321257402eb4c98e23191f2cd77f84fdd
Garming <Garming@example.com>
2026-04-06 14:14:06 +0800

    requests库学习-爬虫代码结构

--- test_study.py
+++ test_study.py
@@ -1,8 +1,11 @@
+import time
+
 import requests
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
-
+    "content-type":
+    "application/x-www-form-urlencoded; charset=UTF-8"
 }
 
 url = "https://passport2.chaoxing.com/fanyalogin"
@@ -21,10 +24,41 @@ data = {
 }
 
 session = requests.Session()
-r = session.post(url, data = data, headers = headers, timeout = 10)
 
-print(r.status_code)
-print(r.text[:500])
+print("正在登录")
+r = session.post(url, data = data, headers = headers, timeout = 10)
+if r.json()["status"]:  #json()转换json数据为python的字典和列表
+    print("成功")
+else:
+    print("失败")
+    exit()  #登陆失败，程序要终止
+
+time.sleep(2)#模拟人浏览网页速度
+
+#搜索“区块链”课程
+url2 = "https://mooc2-ans.chaoxing.com/mooc2-ans/visit/courselistdata"
+
+data = {
+    "courseType": 1,
+    "courseFolderId": 0,
+    "query": "区块链",
+    "pageHeader": -1,
+    "single": 0,
+    "superstarClass": 0,
+    "isFirefly": 0
+}
+
+try:
+    c_r = session.post(url2,data = data, headers = headers, timeout = 10)
+    print("搜索成功")
+    print(r.status_code)
+    print(r.text[:500])
+except Exception as e:
+    print("搜索失败", e)
+
+time.sleep(2)
+
+
 
 
 
